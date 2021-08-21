03bffd9fe851a53c6093aaea348b840aaf4cf4a5
Sutjapong <sutjapong@conceptreach.com>
2021-08-21 20:09:03 +0700

    first

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <vue-editor v-model="content"></vue-editor>
+        {{ content }}
+    </div>
+
+</template>
+
+<style scope>
+
+</style>
+
+<script>
+import { VueEditor, Quill } from "vue3-editor";
+
+    export default {
+        components: {
+            VueEditor,
+            Quill
+        },
+
+        props: {
+
+        },
+        mounted() {
+
+        },
+        data(){
+            return {
+                content: "<h1>Some initial content</h1>"
+            }
+        },
+        methods:{
+
+        },
+        computed:{
+
+        }
+    }
+</script>
+
+
